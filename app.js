185af786e5be292c291d782d820083d25a06b170
QusaiQeisi <>
2021-04-18 20:44:48 +0000

    qu

--- app.js
+++ app.js
@@ -1,45 +1,4 @@
-<<<<<<< HEAD
-// //console.log ('qusai')
-// alert('welcom to wadiram');
 
-// //var userwadiRam;
-
-// //var wadiRum = function(){
-
-// var wadiRam= prompt('please enter the camp name?!');
-
-// var wadiRum= prompt('please enter your time?!');
-// //}
-// //wadiRum ();
-// if(wadiRum >= 18){
-//   document.write('Good Evening');
-//   document.write('<img src="https://cf.bstatic.com/images/hotel/max1024x768/159/159016236.jpg">');
-// }else if(wadiRum >= 12){
-//   document.write('Good Afternoon');
-//   document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkd1K_9Taf2Z4Wh95Hd8kfkXpigEQFFYBSUg&usqp=CAU">')
-// }else if (wadiRum >= 0){
-//   document.write('Good Morning');
-//   document.write('<img src="https://cf.bstatic.com/images/hotel/max1024x768/236/236994988.jpg">')
-// }
-// while(wadiRam !== 'mars'&& wadiRam !== 'bedouin'){
-//   wadiRam = prompt('Please enter the name mars or bedouin');
-// }
-
-// var userChoose;
-
-
-// if(wadiRam === 'mars'){
-// userChoose='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoak-JfCYdONQ2EiwN3xl3cGaeqvmjqEi4Zw&usqp=CAU">';
-// }else if (wadiRam === 'bedouin'){
-//   userChoose='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBw6YJR55FzWF2dDjgluO-ns4qyEi80RKDAA&usqp=CAU">';
-// }
-
-// var imagesNumber=prompt('how many image?');
-// 	for(var i=0 ; i < imagesNumber; i++){
-// 	 document.write(userChoose);
-//   }
-
-=======
 //console.log ('qusai')
 alert('welcom to wadiram');
 
@@ -74,4 +33,4 @@ var imagesNumber=prompt('how many image?');
 	for(var i=0 ; i < imagesNumber; i++){
 	 document.write(userChoose);
   }
->>>>>>> dfec4a289024c2b4a9bcf10c0c42abddcb9a213a
+
